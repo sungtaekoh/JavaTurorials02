92267c5889ad997f25fd36a4483d62199692e345
brown <ohbass95@gmail.com>
2021-11-21 23:21:03 +0900

    자바공부1

---
+++ src/ClasspathDemo2.java
@@ -0,0 +1,16 @@
+class Item2{
+	//클래스 패스
+	public void print() {
+		System.out.println("Hello world");
+	}
+}
+
+
+public class ClasspathDemo2 {
+	
+	public static void main(String[] args) {
+		Item2 i1 = new Item2();
+		
+		i1.print();
+	}
+}
